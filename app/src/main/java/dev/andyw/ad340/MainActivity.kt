757632de6966730bb3b6e3dba0419181dd0376f8
andylwilson <andy787@gmail.com>
2020-05-07 01:08:03 -0700

    Implements RecyclerView and RecyclerView Adapter, adds view holder, layout for item views, connects binding code and adds clicklisteners and touch feedback, adds pseudo randomization in repository.

--- app/src/main/java/dev/andyw/ad340/MainActivity.kt
+++ app/src/main/java/dev/andyw/ad340/MainActivity.kt
@@ -4,11 +4,15 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.LinearLayout
 import android.widget.Toast
+import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
 
-    // region Setup Methods
+    private val forecastRepository = ForecastRepository()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,36 +27,24 @@ class MainActivity : AppCompatActivity() {
             if (zipcode.length != 5) {
                 Toast.makeText(this, R.string.zipcode_entry_error, Toast.LENGTH_SHORT).show()
             } else {
-                Toast.makeText(this, "Your zipcode is " + zipcode, Toast.LENGTH_SHORT).show()
+                forecastRepository.loadForecast(zipcode)
             }
         }
+
+        val forecastList: RecyclerView = findViewById(R.id.forecastList)
+        forecastList.layoutManager = LinearLayoutManager(this)
+
+        val dailyForecastAdapter = DailyForecastAdapter() { forecastItem ->
+            val msg = getString(R.string.forecast_clicked_format, forecastItem.temp, forecastItem.description)
+            Toast.makeText(this, msg, Toast.LENGTH_SHORT).show()
+        }
+        forecastList.adapter = dailyForecastAdapter
+
+        val weeklyForecastObserver = Observer<List<DailyForecast>> { forecastItems ->
+            //update our list adapter
+            dailyForecastAdapter.submitList(forecastItems)
+
+        }
+        forecastRepository.weeklyForecast.observe(this, weeklyForecastObserver)
     }
-
-    override fun onStart() {
-        super.onStart()
-    }
-
-    override fun onResume() {
-        super.onResume()
-    }
-
-    // endregion Setup Methods
-
-    // region Teardown Methods
-
-    override fun onPause() {
-        super.onPause()
-    }
-
-    override fun onStop() {
-        super.onStop()
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-    }
-
-    // endregion Teardown Methods
-
-
 }
